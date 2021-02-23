c4762aaab8b1691d92a4c4cee8295d5b5affa464
sonal <sonalkarle01@gmail.com>
2021-02-23 21:37:16 +0530

    Use reflection to create mood analyser with parameter constructor

--- MoodAnalyserDay12/Moodfactory.cs
+++ MoodAnalyserDay12/Moodfactory.cs
@@ -1,59 +1,73 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Reflection;
-using System.Text;
 using System.Text.RegularExpressions;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Text;
 
-namespace MoodanalyserDay12
+namespace MoodAnalyserDay12
 {
-    public class Moodfactory
+    public class MoodAnalyserfactory
     {
-        public static object CreateMoodAnalyserObject(string className, string constructor)
+        /// <summary>
+        /// UC-4 CreateMoodAnalyse method create object of MoodAnalyse class 
+        /// </summary>
+        /// <param name="className"></param>
+        /// <param name="constructorName"></param>
+        /// <returns></returns>
+        public static object CreateMoodAnalyse(string className, string constructorName)
         {
-            string pattern = @"  " + constructor + "$";
-            var result = Regex.Match(className, pattern);
+            //Create pattern to check classname and constructor name should be same
+            string pattern = @"." + constructorName + "$";
+            Match result = Regex.Match(className, pattern);
+
             if (result.Success)
             {
                 try
                 {
-
-                    Type type = Type.GetType("MoodanalyserDay12.Moodanalyser");
-                    var res = Activator.CreateInstance(type);
-                    return res;
-
+                    Assembly assembly = Assembly.GetExecutingAssembly();
+                    Type moodAnalyseType = assembly.GetType(className);
+                    return Activator.CreateInstance(moodAnalyseType);
                 }
-                catch (NullReferenceException)
+                catch (ArgumentNullException)
                 {
-                    throw new moodanalyserCustomeexception(moodanalyserCustomeexception.Exceptiontype.NO_SUCH_CLASS, "No such class found");
+                    throw new moodanalysercustomException(moodanalysercustomException.ExceptionType.NO_SUCH_CLASS, "Class not found");
+
                 }
             }
             else
             {
-                throw new moodanalyserCustomeexception(moodanalyserCustomeexception.Exceptiontype.NO_SUCH_CONSTRUCTOR, "No such constructor found");
+                throw new moodanalysercustomException(moodanalysercustomException.ExceptionType.NO_SUCH_METHOD, "Method not found");
             }
         }
-        public static object CreateMoodanalyseParameterizedConstructor(string className, string constructor, string message)
+
+        /// <summary>
+        /// UC-5 For parameterised constructor by pssing messge parameter to the class method
+        /// </summary>
+        /// <param name="className"></param>
+        /// <param name="constructorName"></param>
+        /// <returns></returns>
+        public static object CreateMoodAnalyseUsingParameterizedConstructor(string className, string constructorName)
         {
-            Type type = typeof(MoodanalyserDay12.Moodanalyser);
+            Type type = typeof(MoodAnalyser);
             if (type.Name.Equals(className) || type.FullName.Equals(className))
             {
-                if (type.Name.Equals(constructor))
+                if (type.Name.Equals(constructorName))
                 {
                     ConstructorInfo construct = type.GetConstructor(new[] { typeof(string) });
-                    object obj = construct.Invoke(new object[] { message });
-                    return obj;
+                    object instance = construct.Invoke(new object[] { "HAPPY" });
+                    return instance;
                 }
+
                 else
                 {
-
-                    throw new moodanalyserCustomeexception(moodanalyserCustomeexception.Exceptiontype.NO_SUCH_CONSTRUCTOR, "Constructor Not found");
+                    throw new moodanalysercustomException(moodanalysercustomException.ExceptionType.NO_SUCH_METHOD, "Method not found");
                 }
             }
             else
             {
-                throw new moodanalyserCustomeexception(moodanalyserCustomeexception.Exceptiontype.NO_SUCH_CLASS, "Class is Not found");
+                throw new moodanalysercustomException(moodanalysercustomException.ExceptionType.NO_SUCH_CLASS, "Class not found");
             }
-
         }
+
     }
 }
--- MoodAnalyserTest/UnitTest1.cs
+++ MoodAnalyserTest/UnitTest1.cs
@@ -1,33 +1,134 @@
 using NUnit.Framework;
+using MoodAnalyserDay12;
 
-using MoodanalyserDay12;
 
-namespace MoodAnalyserTest
+namespace NUnitTestProject
 {
+
     public class Tests
     {
 
+        MoodAnalyserfactory moodAnalyserfactory;
         [SetUp]
         public void Setup()
         {
+            moodAnalyserfactory = new MoodAnalyserfactory();
 
         }
 
+        /// <summary>
+        /// TC-4.1 Given MoodAnalyse Class Name Should Return MoodAnalyser Object
+        /// </summary>
         [Test]
-        public void Given_moodanalyserReflection_return_Moodanalyserobject()
+        public void MoodAnalyserClassName_ShouldReturnMoodAnalyserObject()
         {
-            Moodanalyser obj = new Moodanalyser();
-            object result = Moodfactory.CreateMoodAnalyserObject("MoodanalyserDay11.Moodanalyser", "Moodanalyser");
-            obj.Equals(result);
+            string message = null;
+            object expected = new MoodAnalyser(message);
+            object obj = MoodAnalyserfactory.CreateMoodAnalyse("MoodAnalyserDay12.MoodAnalyser", "MoodAnalyser");
+            expected.Equals(obj);
         }
+
+        /// <summary>
+        /// TC-4.2 Given MoodAnalyse Class Name When Improper Should Throw Exception
+        /// </summary>
         [Test]
-        public void GivenMoodAnalyser_Reflection_return_MoodanalyserParametarized_object()
+        public void MoodAnalyserClassName_WhenImproper_ShouldThrowMoodAnalyserException()
         {
-            var obj = new Moodanalyser("I am in HAPPY mood");
-            object result = Moodfactory.CreateMoodanalyseParameterizedConstructor("MoodAnalyserDay12.Moodanalyser", "Moodanalyser", "Happy");
-            obj.Equals(result);
+            object obj = null;
+            
+            string expected = "Class not found";
+            try
+            {
+                obj = MoodAnalyserfactory.CreateMoodAnalyse("MoodAnalyserDay12.Mood", "Mood");
+            }
+            catch (moodanalysercustomException exception)
+            {
+                Assert.AreEqual(expected, exception.Message);
+            }
         }
-        
-      
+
+        /// <summary>
+        /// TC-4.3 Given MoodAnalyse Class Name When Constructor is Improper Should Throw Exception
+        /// </summary>
+        [Test]
+        public void MoodAnalyserClassName_WhenConstructorIsImproper_ShouldThrowMoodAnalyserException()
+        {
+            object obj = null;
+            
+            string expected = "Method not found";
+            try
+            {
+                obj = MoodAnalyserfactory.CreateMoodAnalyse("MoodAnalyserDay12.MoodAnalyser", "AnalyserMood");
+            }
+            catch (moodanalysercustomException exception)
+            {
+                Assert.AreEqual(expected, exception.Message);
+            }
+        }
+
+
+        /// <summary>
+        /// TC-5.1  Given MoodAnalyse Class Name Should Return MoodAnalyser Object
+        /// </summary>
+        [Test]
+        public void MoodAnalyserClassName_ShouldReturnMoodAnalyserObject_UsingParametrizedConstructor()
+        {
+            object expected = new MoodAnalyser("HAPPY");
+            object obj = MoodAnalyserfactory.CreateMoodAnalyseUsingParameterizedConstructor("MoodAnalyserDay12.MoodAnalyser", "MoodAnalyser");
+            expected.Equals(obj);
+        }
+
+        /// <summary>
+        /// TC-5.2  Given MoodAnalyse Class Name When Improper Should Throw Exception
+        /// </summary>
+        [Test]
+        public void MoodAnalyserClassName_WhenImproper_UsingParametrizedConstructor_ShouldThrowExcpetion()
+        {
+            string expected = "Class not found";
+            try
+            {
+                object obj = MoodAnalyserfactory.CreateMoodAnalyseUsingParameterizedConstructor("MoodAnalyserDay12.Mood", "MoodAnalyser");
+            }
+            catch (moodanalysercustomException exception)
+            {
+                Assert.AreEqual(expected, exception.Message);
+            }
+        }
+
+        /// <summary>
+        /// TC-5.3  Given MoodAnalyse Class Name When Constructor is Improper Should Throw Exception
+        /// </summary>
+        [Test]
+        public void MoodAnalyserClassName_WhenConstructorIsImproper_UsingParametrizedConstructor_ShouldThrowExcpetion()
+        {
+            string expected = "Method not found";
+            try
+            {
+                object obj = MoodAnalyserfactory.CreateMoodAnalyseUsingParameterizedConstructor("MoodAnalyserDay12.MoodAnalyser", "Mood");
+            }
+            catch (moodanalysercustomException exception)
+            {
+                Assert.AreEqual(expected, exception.Message);
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
