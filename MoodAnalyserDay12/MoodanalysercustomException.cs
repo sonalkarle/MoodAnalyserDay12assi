1f1e175e1e7e10f8a88390fb9e2c256570208c53
sonal <sonalkarle01@gmail.com>
2021-02-23 22:34:03 +0530

    Welcome

---
+++ MoodAnalyserDay12/MoodanalysercustomException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyserDay12
+{
+    public class moodanalysercustomException : Exception
+    {
+        ExceptionType type;
+        public enum ExceptionType
+        {
+            NO_SUCH_CLASS,
+            NO_SUCH_METHOD,
+            EMPTY_MESSAGE,
+            NULL_MESSAGE,
+            NO_SUCH_FIELD,
+
+        }
+
+        public moodanalysercustomException(ExceptionType type, string message) : base(message)
+        {
+            this.type = type;
+        }
+    }
+}
