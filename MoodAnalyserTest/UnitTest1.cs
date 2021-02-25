7d6dd4bd4a42d959eef2b24a33cf7bf29b67d2a2
sonal <sonalkarle01@gmail.com>
2021-02-25 16:17:12 +0530

    Create mood analyse object using Parameterized constructor

--- MoodAnalyserTest/UnitTest1.cs
+++ MoodAnalyserTest/UnitTest1.cs
@@ -55,7 +55,7 @@ namespace NUnitTestProject
         {
             object obj = null;
             
-            string expected = "Method not found";
+            string expected = "Constructor not found";
             try
             {
                 obj = MoodAnalyserfactory.CreateMoodAnalyse("MoodAnalyserDay12.MoodAnalyser", "AnalyserMood");
@@ -101,7 +101,7 @@ namespace NUnitTestProject
         [Test]
         public void MoodAnalyserClassName_ConstructorIsImproper_UsingParametrizedConstructor_Should_ThrowExcpetion()
         {
-            string expected = "Method not found";
+            string expected = "Constructor not found";
             try
             {
                 object obj = MoodAnalyserfactory.CreateMoodAnalyseUsingParameterizedConstructor("MoodAnalyserDay12.MoodAnalyser", "Mood");
